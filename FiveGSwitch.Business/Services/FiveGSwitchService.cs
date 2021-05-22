c1af35311a0cdb4b4030cf08a0417989791970e2
qiuhaotc <qiu66hao@163.com>
2021-05-22 12:27:25 +0800

    添加项目文件。

---
+++ FiveGSwitch.Business/Services/FiveGSwitchService.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Service.QuickSettings;
+
+namespace FiveGSwitch.Business
+{
+    [Service(Name = "com.qh.fivegswitch.FiveGSwitchService",
+             Permission = Android.Manifest.Permission.BindQuickSettingsTile,
+             Label = "@string/tile_name",
+             Icon = "@drawable/five_g_switch")]
+    [IntentFilter(new[] { ActionQsTile })]
+    public class FiveGSwitchService : TileService
+    {
+        bool active;
+
+        public override void OnClick()
+        {
+            base.OnClick();
+
+            active = !active;
+            QsTile.State = active ? TileState.Active : TileState.Inactive;
+            QsTile.UpdateTile();
+        }
+    }
+}
