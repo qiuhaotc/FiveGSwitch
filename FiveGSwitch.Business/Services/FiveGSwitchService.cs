efcc59ea5bd1e68afcbd1ee09f1efdf94b911ec3
qiuhaotc <qiu66hao@163.com>
2021-05-22 21:41:29 +0800

    switch function for MIUI finished

--- FiveGSwitch.Business/Services/FiveGSwitchService.cs
+++ FiveGSwitch.Business/Services/FiveGSwitchService.cs
@@ -1,4 +1,6 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Service.QuickSettings;
 
 namespace FiveGSwitch.Business
@@ -10,15 +12,52 @@ namespace FiveGSwitch.Business
     [IntentFilter(new[] { ActionQsTile })]
     public class FiveGSwitchService : TileService
     {
+        bool capable;
         bool active;
+
+        Lazy<MIUISwitchProvider> provider = new Lazy<MIUISwitchProvider>(() => new MIUISwitchProvider());
 
         public override void OnClick()
         {
             base.OnClick();
 
-            active = !active;
-            QsTile.State = active ? TileState.Active : TileState.Inactive;
-            QsTile.UpdateTile();
+            if (QsTile != null)
+            {
+                provider.Value.Toggle();
+
+                active = provider.Value.IsEnabled;
+                QsTile.State = active ? TileState.Active : TileState.Inactive;
+                QsTile.UpdateTile();
+            }
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(@base);
+            capable = provider.Value.Capable;
+        }
+
+        public override void OnStartListening()
+        {
+            base.OnStartListening();
+
+            if (capable)
+            {
+                if (QsTile != null)
+                {
+                    active = provider.Value.IsEnabled;
+                    QsTile.State = active ? TileState.Active : TileState.Inactive;
+                    QsTile.UpdateTile();
+                }
+            }
+            else
+            {
+                if (QsTile != null)
+                {
+                    QsTile.State = TileState.Unavailable;
+                    QsTile.UpdateTile();
+                }
+            }
         }
     }
 }
---
+++ FiveGSwitch.Business/SwitchProviders/MIUISwitchProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.Widget;
+
+namespace FiveGSwitch.Business
+{
+    public class MIUISwitchProvider : ISwitchProvider
+    {
+        static Lazy<Java.Lang.Class> telephoneManager =
+            new Lazy<Java.Lang.Class>(() =>
+                Java.Lang.Class.ForName("miui.telephony.TelephonyManager")
+            );
+
+        Lazy<Java.Lang.Reflect.Method> getInstanceMethod = new Lazy<Java.Lang.Reflect.Method>(() => telephoneManager.Value.GetMethod("getDefault"));
+        Lazy<Java.Lang.Reflect.Method> getIsFiveGCapableMethod = new Lazy<Java.Lang.Reflect.Method>(() => telephoneManager.Value.GetMethod("isFiveGCapable"));
+        Lazy<Java.Lang.Reflect.Method> getIsUserFiveGEnabled = new Lazy<Java.Lang.Reflect.Method>(() => telephoneManager.Value.GetMethod("isUserFiveGEnabled"));
+        Lazy<Java.Lang.Reflect.Method> setUserFiveGEnabled = new Lazy<Java.Lang.Reflect.Method>(() => telephoneManager.Value.GetMethod("setUserFiveGEnabled", Java.Lang.Boolean.Type));
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return IsEnabledCore(GetManagerInstance());
+            }
+        }
+
+        public bool Capable
+        {
+            get
+            {
+                try
+                {
+                    var manager = GetManagerInstance();
+                    return (getIsFiveGCapableMethod.Value.Invoke(manager) as Java.Lang.Boolean).BooleanValue();
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(Application.Context, $"Error Occur: {ex}", ToastLength.Short).Show();
+
+                    return false;
+                }
+            }
+        }
+
+        public void Toggle()
+        {
+            try
+            {
+                var manager = GetManagerInstance();
+                setUserFiveGEnabled.Value.Invoke(manager, !IsEnabledCore(manager));
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(Application.Context, $"Error Occur: {ex}", ToastLength.Short).Show();
+            }
+        }
+
+        bool IsEnabledCore(Java.Lang.Object manager)
+        {
+            try
+            {
+                return (getIsUserFiveGEnabled.Value.Invoke(manager) as Java.Lang.Boolean).BooleanValue();
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(Application.Context, $"Error Occur: {ex}", ToastLength.Short).Show();
+
+                return false;
+            }
+        }
+
+        Java.Lang.Object GetManagerInstance() => getInstanceMethod.Value.Invoke(telephoneManager.Value);
+    }
+}
